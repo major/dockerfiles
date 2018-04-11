74b136b9d921f4eeba56500990b733ddc219fa48
Major Hayden <major@mhtx.net>
2018-04-11 08:15:54 -0400

    Add remaining bits to consume messages

--- rabbitmq-consumer/consumer.sh
+++ rabbitmq-consumer/consumer.sh
@@ -1,2 +1,26 @@
-#!/bin/bash
-/usr/bin/amqp-consume --url=$BROKER_URL -q foo -c 1 cat
+#!/bin/bash -x
+cat <<"EOF"
+******************************************************************************
+     _    _                        _
+ ___| | _| |_   _ __ ___  __ _  __| |_   _
+/ __| |/ / __| | '__/ _ \/ _` |/ _` | | | |
+\__ \   <| |_  | | |  __/ (_| | (_| | |_| |
+|___/_|\_\\__| |_|  \___|\__,_|\__,_|\__, |
+                                     |___/
+******************************************************************************
+Consuming work from:
+
+    ${BROKER_URL}
+
+EOF
+
+# Consume the message queue
+CMDLINE=$(/usr/bin/amqp-consume --url=$BROKER_URL -q foo -c 1 cat)
+
+# Run the command that was sent
+$CMDLINE
+RETCODE=$?
+
+# Sleep for 5 seconds after the work is complete
+sleep 5
+exit $RETCODE
